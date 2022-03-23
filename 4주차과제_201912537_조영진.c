95c277e475b7177645cf7708e3cfc716655557bc
YoungJin <cyjinstar@naver.com>
2022-03-23 16:50:57 +0900

    수정끝
    주석 변경 및 텍스트 출력 변경

--- 4주차과제_201912537_조영진.c
+++ 4주차과제_201912537_조영진.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-void bubble_sort(int arr[])    // 매개변수로 정렬할 배열과 요소의 개수를 받음
+void bubble_sort(int arr[])    // 버블 정렬 함수 정렬
 {
     int temp;
 
     for (int i = 0; i < 6; i++)    // 요소의 개수만큼 반복
     {
-        for (int j = 0; j < 5; j++)   // 
+        for (int j = 0; j < 5; j++)   // 요소의 개수 -1 만큼 반복
         {
             if (arr[j] > arr[j + 1])          // 현재 요소의 값과 다음 요소의 값을 비교하여
             {                                 // 큰 값을
@@ -18,10 +18,12 @@ void bubble_sort(int arr[])    // 매개변수로 정렬할 배열과 요소의 
         }
     }
     printf("[");
-    for (int c = 0; c<6; c++){
+    for (int c = 0; c < 5; c++){ //정렬된 값 출력
         printf("%d, ",arr[c]);
+        if(c == 4){
+            printf("%d]",arr[5]);
+        }
     }
-    printf("]");
 }
 
 
