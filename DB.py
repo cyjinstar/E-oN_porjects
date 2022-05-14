3f198a8bf2c3d18c88829242d94dbda279cf4c84
YoungJin <cyjinstar@naver.com>
2022-05-14 10:16:45 +0900

    Last_week

--- DB.py
+++ DB.py
@@ -1,3 +1,7 @@
-with open('input.txt','rt') as f: #데이터베이스 파일을 가져오는 모듈
-    BookLists = f.readlines()
-BookLists = [line.rstrip('\n') for line in BookLists]
+try:
+    with open('TrainList.txt','rt') as f: #데이터베이스 파일을 가져오는 모듈
+        TrainList = f.readlines()
+    TrainList = [line.rstrip('\n') for line in TrainList]
+    del TrainList[0]
+except:
+    print("TrainList.txt가 없거나 손상되었습니다.")
--- System.py
+++ System.py
@@ -1,79 +1,75 @@
+from posixpath import split
 import DB
 
-#7주차_도서관리_프로그램
+#마지막주차_기차표예매_프로그램
 
 class Menu : #메뉴 클래스
     def __init__(self) -> None:
         pass
 
-    def Select(BookLists) :
-        print("1. 도서 추가")
-        print("2. 도서 검색")
-        print("3. 도서 정보 수정")
-        print("4. 도서 삭제")
-        print("5. 현재 총 도서 목록 출력")
-        print("6. 저장")
-        print("7. 프로그램 종료\n")
+    def Select(TrainList) :
+        print("1. 빠른시간 기차 검색 및 예매")
+        print("2. 전체 기차 시간표 출력")
+        print("3. 나의 예매")
+        print("4. 프로그램 종료\n")
         
         num = int(input("목록에서 수행할 기능을 입력하세요. : "))
         if num == 1 :
-            Book_def.book_append()
+            Train_def.Train_search(TrainList)
         elif num == 2 : 
-            Book_def.book_search()
+            Train_def.Train_print(TrainList)
         elif num == 3 : 
-            Book_def.book_rewrite()
+            Train_def.Train_book()
         elif num == 4 : 
-            Book_def.book_del()
-        elif num == 5 : 
-            Book_def.book_print()
-        elif num == 6 : 
-            Book_def.book_save()
-        elif num == 7 : 
-            Book_def.book_save()
+            Train_def.Train_save()
             exit()
-        else : print("잘못된 명령입니다.")
+        else : print("\n잘못된 명령입니다.\n")
+        Menu.Select(DB.TrainList)
 
-class Book_def : #도서 관리 클래스
+class Train_def : #도서 관리 클래스
     def __init__(self) -> None:
         pass
 
-    def book_append(): #도서 추가 함수
-        a = str(input(" 도서명 저자 출판연도 출판사명 장르 양식으로 입력하세요.\n"))
-        DB.BookLists.append(a)
-
-    def book_search() : #도서 검색 함수
-        inum = int(input("1. 도서명\n2. 저자\n3. 출판연도\n4. 장르\n"))
-        searchString = str(input("검색어를 입력하세요.\n"))
-        for i in DB.BookLists:
-            booklist = i.split()
-            for number in range(len(i.split())):
-                if (number+1) % inum == 0 :
-                    if booklist[number] == searchString :
-                        print(i)
-
-    def book_rewrite() : #도서 수정 함수
-        ListIndex = int(input("목록의 수정할 도서 번호를 입력하세요 : "))
-        print(DB.BookLists[ListIndex-1])
-        a = str(input(" 도서명 저자 출판연도 출판사명 장르 양식으로 입력하세요. \n"))
-        del DB.BookLists[ListIndex-1]
-        DB.BookLists.insert(ListIndex-1, a)
-
-    def book_del() : #도서 제거 함수
-        ListIndex = int(input("목록에서 다음 순서의 도서를 제거 : "))
-        del DB.BookLists[ListIndex-1]
-
-    def book_print() : #도서 출력 함수
+    def Train_print(Tlist) : #전체 기차표 출력 함수
         inx = 1
-        for i in DB.BookLists:
-            print(inx,".",i,"\n")
-            inx+=1
+        print("\n출발시간,출발지,도착지,열차종류,잔여좌석수")
+        for i in Tlist:
+            splitList = i.split()
+            for number in range(len(i.split())):
+                if (number+1) % 5 == 0 :
+                    if splitList[number+1] == "0" :
+                        splitList[number+1] = "매진"
+            print(' '.join(splitList))
 
-    def book_save() : #도서 저장 함수
-        with open('input.txt','wt') as f:
-            for i in DB.BookLists :
+    def Train_search(Tlist) :
+        try :
+            print("열차 검색")
+            searchlist = []
+            searchKey = (input("시간(00:00) 출발역 도착역 열차 종류를 입력해주세요")).split()
+            s = searchKey[0].replace(":","").lstrip("0")
+            for i in Tlist:
+                splitList = i.split()
+                for number in range(len(i.split())):
+                    if number == 0 :
+                        sn = splitList[number].replace(":","").lstrip("0")
+                        if int(sn) >= int(s) :
+                            searchlist.append(' '.join(splitList))
+                    
+
+        except IndexError :
+            print("잘못된 양식입니다!")
+            Menu.Select(DB.TrainList)
+
+    
+    def Train_book() :
+        pass
+
+    def Train_save() : 
+        with open('TrainList.txt','wt') as f:
+            f.writelines("시간_출발_도착_열차종류_잔여좌석수\n")
+            for i in DB.TrainList :
                 f.writelines(i)
                 f.write("\n")
 
-while 1 :
-    print("메뉴")
-    Menu.Select(DB.BookLists)
+
+Menu.Select(DB.TrainList)
