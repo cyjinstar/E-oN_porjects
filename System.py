cb63f8fa7314349a9479876f77cb385afe744aa0
YoungJin <cyjinstar@naver.com>
2022-05-17 00:20:49 +0900

    _2ver

--- System.py
+++ System.py
@@ -1,5 +1,6 @@
 from posixpath import split
 import DB
+import time
 
 #마지막주차_기차표예매_프로그램
 
@@ -13,9 +14,10 @@ class Menu : #메뉴 클래스
         print("3. 나의 예매")
         print("4. 프로그램 종료\n")
         
-        num = int(input("목록에서 수행할 기능을 입력하세요. : "))
+        inum = input("목록에서 수행할 기능을 입력하세요. : ")
+        num = int(inum)
         if num == 1 :
-            Train_def.Train_search(TrainList)
+            print(Train_def.Train_search(TrainList))
         elif num == 2 : 
             Train_def.Train_print(TrainList)
         elif num == 3 : 
@@ -26,7 +28,7 @@ class Menu : #메뉴 클래스
         else : print("\n잘못된 명령입니다.\n")
         Menu.Select(DB.TrainList)
 
-class Train_def : #도서 관리 클래스
+class Train_def : #
     def __init__(self) -> None:
         pass
 
@@ -44,25 +46,34 @@ class Train_def : #도서 관리 클래스
     def Train_search(Tlist) :
         try :
             print("열차 검색")
-            searchlist = []
             searchKey = (input("시간(00:00) 출발역 도착역 열차 종류를 입력해주세요")).split()
             s = searchKey[0].replace(":","").lstrip("0")
+            searchlist = []
+            searchlist.clear()
+            cnt = 0
             for i in Tlist:
                 splitList = i.split()
-                for number in range(len(i.split())):
-                    if number == 0 :
-                        sn = splitList[number].replace(":","").lstrip("0")
-                        if int(sn) >= int(s) :
-                            searchlist.append(' '.join(splitList))
-                    
+                if splitList[1] == searchKey[1] and splitList[3] == searchKey[2] :
+                    if splitList[4] == searchKey[3] : 
+                        for number in range(len(i.split())):
+                            if number == 0 :
+                                sn = splitList[number].replace(":","").lstrip("0")
+                                if int(sn) >= int(s) and int(splitList[5]) != 0:
+                                    searchlist.append(' '.join(splitList))
+                                    print(i)
+            cnt = Tlist.index(searchlist[0])
+            print(cnt, searchlist[0])
+            return int(cnt)
 
         except IndexError :
             print("잘못된 양식입니다!")
+            time.sleep(1)
             Menu.Select(DB.TrainList)
-
     
     def Train_book() :
-        pass
+        reservation = int(input("위 열차로 예매하시겠습니까? yes = 1 no = 0\n"))
+        if reservation :
+            pass
 
     def Train_save() : 
         with open('TrainList.txt','wt') as f:
@@ -72,4 +83,8 @@ class Train_def : #도서 관리 클래스
                 f.write("\n")
 
 
-Menu.Select(DB.TrainList)
+try : Menu.Select(DB.TrainList)
+
+except : 
+    print("\n잘못된 양식입니다. 올바른 값을 선택해주십쇼.\n")
+    Menu.Select(DB.TrainList)
